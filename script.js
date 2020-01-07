e723b1745d193754cc396bcc13e988136a639ea4
xazzer81 <achau1019@gmail.com>
2020-01-07 16:01:57 -0500

    Added a little more functionality

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Math functions
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -10,6 +11,7 @@ function multiply (num1, num2) {
 function divide (num1, num2) {
     return num1 / num2;
 }
+// Operate function takes two nums and finds the suitable function for operation
 function operate (operator, num1, num2){
     switch (operator){
         case '+':
@@ -22,9 +24,11 @@ function operate (operator, num1, num2){
             return divide(num1, num2);   
     }
 }
+// function calls displayClick function on digit returned from button click
 function eventFunc (event){
     displayClick(event.target.textContent);
 }
+// displays clicked button on the calculator screen.
 function displayClick(digit){
     let display = document.querySelector('.displayScreen');
     if (display.textContent.length < 15){
@@ -33,9 +37,27 @@ function displayClick(digit){
     }
 }
 
+// script start
 let displayVal = '';
 
 const digits = document.querySelectorAll('.digit');
 digits.forEach(digit => {
     digit.addEventListener('click', eventFunc);
 });
+
+const operatorBtns = document.querySelectorAll('.operator');
+operatorBtns.forEach(btn => {
+    btn.addEventListener('click', event => {
+        event.target.style.backgroundColor = 'rgb(192, 108, 108)';
+        // remove event handler from rest of .operators
+        // return which event handler clicked.
+    });
+});
+
+// take the displayScreen text content convert it into number
+// make variable to store that number
+// reset the screen 
+// display another number, store it another variable
+// take operator
+// call operate function 
+// display the results in display window.
